f8facdce205a3716ce39d0b6bbd0a6bb938392dc
Facundo Baynac <73485212+flbaynac@users.noreply.github.com>
2023-08-30 18:17:42 -0300

    Update autoinstall-wsrvvm.ps1

--- autoinstall-wsrvvm.ps1
+++ autoinstall-wsrvvm.ps1
@@ -2,23 +2,22 @@
 .NOTES
     Author         : Facundo Baynac facundobaynac.com.ar
     GitHub         : https://github.com/flbaynac
-    Version        : 00.01
     License        : MIT
 #>
 
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Output "Winutil needs to be run as Administrator. Attempting to relaunch."
+    Write-Output "Se necesita permisos de administrador para habilitar las características. Re-lanzando con permisos."
     Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "iwr -useb https://raw.githubusercontent.com/flbaynac/winsrvvm/main/autoinstall-wsrvvm.ps1 | iex"
     break
 }
 
-"Habilitando Hyper-v (Debe reiniciar una vez instalada la característica)"
+"Habilitando Hyper-v (Debe reiniciar una vez instalada la caracteristica)"
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 
 "Descargando Windows Server 2022 Trial"
 iwr -useb https://go.microsoft.com/fwlink/p/?linkid=2195333 -OutFile wsrv2022.iso
 
-"Creando la máquina virtual"
+"Creando la maquina virtual"
 $VMName = "WINSRV2022SO2"
 $Switch = 'External VM Switch'
 $InstallMedia = '.\wsrv2022.iso'
@@ -36,3 +35,5 @@ Set-VMFirmware -VMName $VMName -FirstBootDevice $DVDDrive
 
 # Activar virtualización anidada
 Set-VMProcessor -VMName $VMName -ExposeVirtualizationExtensions $True
+
+"Abra el Administrador de Hyper-v y arranque la maquina virtual, siga los pasos del instalador del sistema"
